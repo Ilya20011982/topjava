38d0050f13b836fbc5f8b3e5277a3dcc28e06451
Ilya20011982 <Ilya_fr@mail.ru>
2022-08-10 19:17:39 +0500

    HW_9 p.1

--- src/main/java/ru/javawebinar/topjava/to/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/to/MealTo.java
@@ -1,18 +1,26 @@
 package ru.javawebinar.topjava.to;
 
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.beans.ConstructorProperties;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
 public class MealTo extends BaseTo {
+    @NotNull
+    private LocalDateTime dateTime;
 
-    private final LocalDateTime dateTime;
+    @NotBlank
+    @Size(min = 2, max = 120)
+    private String description;
 
-    private final String description;
+    @Range(min = 10, max = 5000)
+    private int calories;
 
-    private final int calories;
-
-    private final boolean excess;
+    private boolean excess;
 
     @ConstructorProperties({"id", "dateTime", "description", "calories", "excess"})
     public MealTo(Integer id, LocalDateTime dateTime, String description, int calories, boolean excess) {
